5898eba116eeac1c95d23f970891a31998382852
ulan <umanasov24@gmail.com>
2019-12-18 19:20:22 -0600

    added zoneid

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "bucket_name" {
     value = aws_s3_bucket.b.bucket
 } 
 output "aws_security_group" { 
-    value = aws_security_group.allow_tls.bucket_name
+    value = aws_security_group.allow_tls.name
 } 
 output "aws_key_pair" { 
     value = aws_key_pair.deployer.key_name
---
+++ route53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "www" { 
+  zone_id = "Z11HUQ2CPZZDNC" 
+  name    = "www.ulan-best.com." 
+  type    = "A" 
+  ttl     = "60" 
+  records = [aws_instance.web.public_ip] 
+} 
