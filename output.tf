26599b8e6d2e9d89e885fa8d40cd201c13bfa71a
ulan <umanasov24@gmail.com>
2019-12-18 19:09:53 -0600

    added

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
 output "bucket_name" { 
     value = aws_s3_bucket.b.bucket
 } 
+output "sec_group" { 
+    value = aws_security_group.allow_tls.bucket_name
+} 
+output "aws_key_pair" { 
+    value = aws_key_pair.deployer.key_name
+} 
+output "username" { 
+    value = "some_username"
+} 
+output "password" { 
+    value = "password"
+} 
