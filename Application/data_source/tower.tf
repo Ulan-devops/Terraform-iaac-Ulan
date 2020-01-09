a386857308a1395c84e5648a1e8ea8e50bdd2123
ulan <umanasov24@gmail.com>
2020-01-08 20:03:02 -0600

    added route53

--- Application/data_source/tower.tf
+++ Application/data_source/tower.tf
@@ -1,8 +1,7 @@
 # Go to us-east-2 instances. Wt create with the latest  ami from scratch a new instance called Hello World
 provider "aws" {
-    region = "us-east-1" 
-} 
-
+    region = "us-east-1"
+    }
 # search for Ubuntu latest with the owner
 data "aws_ami" "centos" {
     filter {
@@ -17,15 +16,13 @@ data "aws_ami" "centos" {
     owners = ["679593333241"]
     }
 # Show the latest ami id
-output "ami" { 
+output "ami" {
     value = data.aws_ami.centos.id
-} 
-
-resource "aws_key_pair" "towerkey" { 
-  key_name   = "towerkey" 
-  public_key = file("~/.ssh/id_rsa.pub")
-} 
-
+    }
+resource "aws_key_pair" "towerkey" {
+    key_name   = "towerkey"
+    public_key = file("~/.ssh/id_rsa.pub")
+    }
 
 # so data source collects us a new ami
 
@@ -44,16 +41,16 @@ resource "aws_instance" "tower" {
                 "sudo yum install -y epel-release",
                 ]
                 }
-                tags = {
-    Name = "HelloWorld"
-    }
-    }
+    tags = {
+        Name = "HelloWorld"
+        }
+        }
 # here we added a new route53 record and hosted zone id from your route53
 
-resource "aws_route53_record" "tower" { 
-  zone_id = "Z11HUQ2CPZZDNC" 
-  name    = "tower.example.com" 
-  type    = "A" 
-  ttl     = "300" 
-  records = [aws_instance.web.public_ip] 
-} 
+resource "aws_route53_record" "tower" {
+    zone_id = "Z11HUQ2CPZZDNC"
+    name    = "tower.example.com"
+    type    = "A"
+    ttl     = "300"
+    records = [aws_instance.web.public_ip]
+    }
