4d6e9c34d59d2eb8c8fde4197a82741580282f04
ulan <umanasov24@gmail.com>
2020-01-08 18:40:04 -0600

    spaces modifying

--- Application/data_source/ec2.tf
+++ Application/data_source/ec2.tf
@@ -1,15 +1,14 @@
 # Go to us-east-2 instances
-provider "aws" { 
-  region = "us-east-2" 
+provider "aws" {
+    region = "us-east-2" 
 } 
 
 # search for Ubuntu latest with the owner
-data "aws_ami" "ubuntu" { 
-  most_recent = true 
-  owners = ["099720109477"] 
-
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    owners = ["099720109477"]
 } 
 # Show the ami id
 output "ami" { 
-  value = "${data.aws_ami.ubuntu.id}" 
+    value = "${data.aws_ami.ubuntu.id}" 
 } 
