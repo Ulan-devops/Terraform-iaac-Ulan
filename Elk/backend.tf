5c3dc6dfb906d6a09424fdeb4506a7516ddf64fd
ulan <umanasov24@gmail.com>
2020-01-09 21:15:02 -0600

    added

---
+++ Elk/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+   backend "s3" {
+    bucket = "eksnew"
+    region = "us-east-1"
+    key    = "laptop"
+  }
+}
+
---
+++ Elk/key.tf
@@ -0,0 +1,5 @@
+resource "aws_key_pair" "laptop" {
+  key_name   = "laptop"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
