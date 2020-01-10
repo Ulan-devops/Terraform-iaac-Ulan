3239cfddaf8abc58302c91b83219cbc1b507f86c
ulan <umanasov24@gmail.com>
2020-01-09 21:37:39 -0600

    puut

--- Elk/step2/elk.tf
+++ Elk/step2/elk.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = "${var.region}"
-}
-
+}}
 resource "aws_instance" "elk" {
   instance_type               = "${var.instance_type}"
   ami                         = "${var.ami}"
