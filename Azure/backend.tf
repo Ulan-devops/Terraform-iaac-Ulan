c4f55c5c1701c38b9285c681894ce97aba126aad
ulan <umanasov24@gmail.com>
2020-01-18 12:55:03 -0600

    added a subscribtion id

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -4,5 +4,7 @@ terraform {
     storage_account_name = "example1ulan"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
+    subscription_id  = "e25f2a8f-9b53-465f-9365-425ee0e4e70d"
+    tenant_id        = "2617bd0b-d588-42a9-b5f3-a2b431ec33e2"
   }
 }
