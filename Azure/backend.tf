ab8fcf10c0962e413fe7ee34783492a426ec5a6d
ulan <umanasov24@gmail.com>
2020-01-18 12:58:23 -0600

    added

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -1,10 +1,12 @@
+
 terraform {
   backend "azurerm" {
-    storage_account_name = "abcd1234"
+    storage_account_name = "example1ulan"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-    use_msi              = true
-    subscription_id  = "e25f2a8f-9b53-465f-9365-425ee0e4e70d"
-    tenant_id        = "2617bd0b-d588-42a9-b5f3-a2b431ec33e2"
+
+    # rather than defining this inline, the Access Key can also be sourced
+    # from an Environment Variable - more information is available below.
+    access_key = "e25f2a8f-9b53-465f-9365-425ee0e4e70d"
   }
 }
