7c9370cfa6dc397347870a21f8f49917d1fe9895
ulan <umanasov24@gmail.com>
2020-01-18 12:56:27 -0600

    add

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "example1"
-    storage_account_name = "example1ulan"
+    storage_account_name = "abcd1234"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
+    use_msi              = true
     subscription_id  = "e25f2a8f-9b53-465f-9365-425ee0e4e70d"
     tenant_id        = "2617bd0b-d588-42a9-b5f3-a2b431ec33e2"
   }
