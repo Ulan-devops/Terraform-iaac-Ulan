6cdf568515e69cf93cff0e21a8e3d025b2482e55
ulan <umanasov24@gmail.com>
2020-01-18 13:05:38 -0600

    changed

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -5,7 +5,7 @@ terraform {
     storage_account_name = "example1ulan"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-    access_key = "8Sm0tMADe+K9RgnM8FetI0VwFNn4ZfPc8OBYgS5lX2LvCgQ17fMAxe6J30L3vRDPRzC7bATPyuQhec1aKv2eMw=="
+    access_key = ""
   }
 }
     
