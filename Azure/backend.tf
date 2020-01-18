cb23816b668cbf4e8dab7e6daf8c33cdd28b569d
ulan <umanasov24@gmail.com>
2020-01-18 13:00:54 -0600

    added access key

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -1,12 +1,11 @@
 
 terraform {
   backend "azurerm" {
-    storage_account_name = "example1ulan"
+    resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "abcd1234"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-
-    # rather than defining this inline, the Access Key can also be sourced
-    # from an Environment Variable - more information is available below.
-    access_key = "e25f2a8f-9b53-465f-9365-425ee0e4e70d"
+    access_key = ""
   }
 }
+    
