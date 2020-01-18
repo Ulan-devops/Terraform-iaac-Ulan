f47d4ef7513dde7231c275df96d00d1dcaa6ab7d
ulan <umanasov24@gmail.com>
2020-01-18 13:03:11 -0600

    add

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -1,8 +1,8 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "StorageAccount-ResourceGroup"
-    storage_account_name = "abcd1234"
+    resource_group_name  = "example1"
+    storage_account_name = "example1ulan"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
     access_key = "8Sm0tMADe+K9RgnM8FetI0VwFNn4ZfPc8OBYgS5lX2LvCgQ17fMAxe6J30L3vRDPRzC7bATPyuQhec1aKv2eMw=="
